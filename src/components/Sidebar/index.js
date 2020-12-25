b66264173914eebf47a02f7e9a6ad18c7b0338d3
Phan Nhật Vinh <phannhatvinh10a@gmail.com>
2020-12-26 00:36:27 +0700

    Updated sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import { Layout, Menu, Divider } from 'antd';
+import { Layout, Menu, Divider, Progress, Typography } from 'antd';
 import
 {
   CalendarOutlined,
@@ -9,7 +9,7 @@ import
   SettingOutlined,
 
 } from '@ant-design/icons';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { history } from '../../history'
 
@@ -19,6 +19,8 @@ function Sidebar(props)
 {
 
   const [collapsed, setCollapsed] = useState(false);
+  const [openKeys, setOpenKeys] = useState(['sub1', 'sub2']);
+  const rootKeys = ['sub1', 'sub2'];
 
   const onCollapse = () =>
   {
@@ -37,38 +39,65 @@ function Sidebar(props)
 
   }
 
+  useEffect(() =>
+  {
+    if (!collapsed)
+    {
+      setOpenKeys(rootKeys);
+    }
+  }, [collapsed]);
+
+  const onOpenKeysChanged = (items) =>
+  {
+    console.log(items)
+    const latestOpenKey = items.find(key => openKeys.indexOf(key) === -1);
+    if (rootKeys.indexOf(latestOpenKey) === -1)
+    {
+      setOpenKeys(items);
+    } else
+    {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : rootKeys);
+    }
+  }
+
   return (
     <div style={{ minHeight: '100vh' }}>
       <Sider className="sidebar" collapsible collapsed={collapsed} theme="light" width="15vw" onCollapse={onCollapse} style={{ minHeight: '100vh' }}>
-        <Menu className='sidebar-menu' theme="light" defaultSelectedKeys={['1']} mode="inline">
+        <Menu inlineIndent={24} onOpenChange={onOpenKeysChanged} className='sidebar-menu' theme="light" openKeys={openKeys} defaultOpenKeys={openKeys} defaultSelectedKeys={['1']} mode="inline">
           <Link to='/'>
-            <img id="sidebar-logo" style={{ display: 'block', margin: 'auto', marginBottom: "32px", marginTop: '10px', transitionDuration: '200ms' }} src="logo.svg" alt="logo" />
+            <img id="sidebar-logo" style={{ display: 'block', margin: 'auto', marginBottom: "32px", marginTop: '20px', transitionDuration: '200ms' }} src="logo.svg" alt="logo" />
           </Link>
-          <Menu.Item className='sidebar-item' key="1" onClick={() => { history.push('/dashboard') }} style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<HomeOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
-            Tổng quan
+          <Menu.SubMenu key="sub1" title="Công việc" icon={collapsed ? <HomeOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} /> : ""}>
+            <Menu.Item className='sidebar-item' key="1" onClick={() => { history.push('/dashboard') }} style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<HomeOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
+              Tổng quan
             </Menu.Item>
-          <Menu.Item key="2" onClick={() => { history.push('/courses') }} style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<BookOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
-            Môn học
+            <Menu.Item key="2" onClick={() => { history.push('/courses') }} style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<BookOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
+              Môn học
             </Menu.Item>
-          <Menu.Item key="3" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<CalendarOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
-            Sự kiện
+            <Menu.Item key="3" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<CalendarOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
+              Sự kiện
             </Menu.Item>
-          <Divider />
-          <Menu.Item key="4" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<MessageOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
-            Tin nhắn
-            </Menu.Item>
-          <Menu.Item key="5" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<UserOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
-            Tài khoản
-            </Menu.Item>
-          <Menu.Item key="6" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<SettingOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
-            Cài đặt
-            </Menu.Item>
-          <Menu.Item key="7" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }}>
+          </Menu.SubMenu>
 
-          </Menu.Item>
-          <Menu.Item key="8" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }}>
-          </Menu.Item>
+          <Divider />
+
+          <Menu.SubMenu key="sub2" title="Cá nhân" icon={collapsed ? <UserOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} /> : ""}>
+            <Menu.Item key="4" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<MessageOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
+              Tin nhắn
+            </Menu.Item>
+            <Menu.Item key="5" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<UserOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
+              Tài khoản
+            </Menu.Item>
+            <Menu.Item key="6" style={{ height: "50px", fontSize: '16px', margin: '15px 0px' }} icon={<SettingOutlined className="sidebar-icons" style={{ marginRight: "30px", fontSize: '16px' }} />}>
+              Cài đặt
+            </Menu.Item>
+          </Menu.SubMenu>
+          <Divider />
         </Menu>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px', visibility: collapsed ? "hidden" : "visible" }}>
+          <Progress type="circle" width={80} percent={75} />
+          <Typography.Paragraph style={{ marginTop: '15px' }}>Nội dung đã hoàn thành</Typography.Paragraph>
+        </div>
       </Sider>
     </div>
   );
